3ff3163b04fe545d64ecde078419152e19a52a60
MarkStudiosOfficial <matty@markstudios.co.uk>
2014-10-15 23:34:25 +0100

    HELP

--- GameName1/Properties/AssemblyInfo.cs
+++ GameName1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameName1")]
-[assembly: AssemblyProduct("GameName1")]
+[assembly: AssemblyTitle("TTG-Game")]
+[assembly: AssemblyProduct("TTG-Game")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Still No Name Yet")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
